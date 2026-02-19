371d04f62582d37a93778e21f858137b45063a39
barron-paw <532394442@qq.com>
2026-02-19 12:38:00 +0800

    Add files via upload

--- frontend/src/data/products.ts
+++ frontend/src/data/products.ts
@@ -1,12 +1,14 @@
 import type { Product } from '../types'
 
+const GOODS_BASE = 'https://api.adulthood.me/adulthoodgoods/123456'
+
 export const placeholderProducts: Product[] = [
   {
     id: 'p1',
     name: 'Product A',
     description: 'Product description placeholder.',
-    images: [],
-    videoUrl: undefined,
+    images: [`${GOODS_BASE}/1.jpg`],
+    videoUrl: `${GOODS_BASE}/2.mp4`,
     defaultVariantId: 'v1',
     variants: [
       { id: 'v1', name: 'Standard', price: '0.00 USDT' },
@@ -17,8 +19,8 @@ export const placeholderProducts: Product[] = [
     id: 'p2',
     name: 'Product B',
     description: 'Product description placeholder.',
-    images: [],
-    videoUrl: undefined,
+    images: [`${GOODS_BASE}/3.jpg`],
+    videoUrl: `${GOODS_BASE}/4.mp4`,
     defaultVariantId: 'v1',
     variants: [
       { id: 'v1', name: 'Single', price: '0.00 USDT' },
@@ -29,8 +31,8 @@ export const placeholderProducts: Product[] = [
     id: 'p3',
     name: 'Product C',
     description: 'Product description placeholder.',
-    images: [],
-    videoUrl: undefined,
+    images: [`${GOODS_BASE}/5.jpg`],
+    videoUrl: `${GOODS_BASE}/6.mp4`,
     defaultVariantId: 'v1',
     variants: [
       { id: 'v1', name: 'Style 1', price: '0.00 USDT' },
